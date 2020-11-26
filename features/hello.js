a98c95593ffb945c61126db01bff23504fbfa597
Soragi <75024476+Soragi@users.noreply.github.com>
2020-11-26 14:37:20 +0100

    Update hello.js

--- features/hello.js
+++ features/hello.js
@@ -2,7 +2,40 @@
 // Respond to various 'hello' words, attach file by URL and from local file system
 var fs = require('fs');
 
+module.exports = function( controller ) {//
+// Respond to various 'hello' words, attach file by URL and from local file system
+var fs = require('fs');
+
 module.exports = function( controller ) {
+
+    controller.hears( [ 'hi','hello','howdy','hey','aloha','hola','bonjour','oi','hallo'], 'message,direct_message', async ( bot,message ) => {
+
+        await bot.reply( message,'Greetings! I am here to help!' );
+        await bot.reply( message, { markdown: '> How can I help you?**\n> - Programs\n> - Incentives\n> - Partner status\n> - Profile settings' } );
+        await bot.reply( message, { markdown: 'Or type `help` to see all available commands' } );
+      });
+
+
+    controller.hears( 'happy boy', 'message,direct_message', async ( bot,message ) => {
+
+        await bot.reply( message, {
+            text: 'Aww!',
+            files: [ 'https://i.pinimg.com/280x280_RS/b0/0e/49/b00e4959dd26367688099678376d4296.jpg' ]
+        });
+    })
+
+    controller.hears( 'local', 'message,direct_message' , async ( bot,message ) => {
+        await bot.reply( message, {
+            text: 'The source code',
+            files: [ fs.createReadStream( './bot.js' ) ]
+        })
+    })
+
+    controller.commandHelp.push( { command: 'hello', text: 'Greetings!' } );
+    controller.commandHelp.push( { command: 'url', text: 'Attach a file via URL' } );
+    controller.commandHelp.push( { command: 'local', text: 'Attach a file from the local file system' } );
+
+}
 
     controller.hears( [ 'hi','hello','howdy','hey','aloha','hola','bonjour','oi' ], 'message,direct_message', async ( bot,message ) => {
 
